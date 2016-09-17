04131380c88291c1f47b6b8314092a496489cf3a
stephenkaraga <stephenkaraga@gmail.com>
2016-09-17 16:38:35 -0400

    Add body-parser middleware and add node_modules to .gitignore

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 
 let port = 3001;
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
 
 app.listen(port, function(){
     console.log('Server is listening on: %s', port);
