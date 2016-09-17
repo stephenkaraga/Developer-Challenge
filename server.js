9b5a441cabbabe075007922d44cc5048d9f95c90
stephenkaraga <stephenkaraga@gmail.com>
2016-09-17 18:54:14 -0400

    Connect to Mongo Database.

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 "use strict"
 
-let port = 3001;
+const log = console.log;
+const port = 3001;
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoClient = require('mongodb').MongoClient
+const mongoURL = 'mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge'
+const app = express();
 
-var app = express();
+mongoClient.connect(mongoURL, function(err, db) {
+  if (err) {
+      log('There was an error connecting to Mongo!');
+      log(err);
+    return;
+  } else {
+    log('Connected to Mongo!');
+  }
+});
+
 
 app.use(bodyParser.urlencoded());
 
 app.use(bodyParser.json());
+
+app.get('/', function(req, res, next) {
+  res.json('Welcome to my solution of the developer challenge!')
+})
 
 app.listen(port, function(){
     console.log('Server is listening on: %s', port);
