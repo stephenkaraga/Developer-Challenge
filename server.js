c8d6a34f8aa8e3645122a81f86b03939d0b00664
stephenkaraga <stephenkaraga@gmail.com>
2016-09-18 08:47:52 -0400

    Set up Monk.

--- server.js
+++ server.js
@@ -7,16 +7,34 @@ const bodyParser = require('body-parser');
 const mongoClient = require('mongodb').MongoClient
 const mongoURL = 'mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge'
 const app = express();
-
+const db = require('monk')(mongoURL)
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+// log(titles)
 mongoClient.connect(mongoURL, function(err, db) {
   if (err) {
       log('There was an error connecting to Mongo!');
       log(err);
     return;
   } else {
+    db.listCollections().toArray(function(err, collections){
+      log(collections);
+    });
     log('Connected to Mongo!');
   }
 });
+
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+  // app.use(function(req, res, next) {
+  //     var err = new Error('Not Found');
+  //     err.status = 404;
+  //     next(err);
+  // });
 
 
 app.use(bodyParser.urlencoded());
@@ -24,7 +42,10 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.get('/', function(req, res, next) {
-  res.json('Welcome to my solution of the developer challenge!')
+  const db = req.db;
+  const titles = db.get('Titles');  
+  titles.find({},{},function(e,docs){
+    log(docs)
 })
 
 app.listen(port, function(){
